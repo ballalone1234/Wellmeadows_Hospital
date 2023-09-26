b2dc56825183e5d549ead7bf00689f994bc1f1b8
ballalone1234 <ballpol.127@gmail.com>
2023-09-26 22:38:09 +0700

    Revert "Page Control Func"
    This reverts commit 141d7bba357b5d1b7ee5c6be3b74e629a0ce2faa.

--- Wellmeadows_Hospital_1101173/MainMenu.vb
+++ Wellmeadows_Hospital_1101173/MainMenu.vb
@@ -1,5 +1,4 @@
 ﻿Public Class MainMenu
-
     Private Sub patienticon_Click_2(sender As Object, e As EventArgs) Handles patienticon.Click
         PatientMenu.Show()
         Me.Hide()
@@ -8,9 +7,4 @@
     Private Sub MainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
-    Private Sub staff_Click(sender As Object, e As EventArgs) Handles stafficon.Click
-        PageControl.NextPage(Me, Staff_menu)
-
-    End Sub
-
 End Class
--- Wellmeadows_Hospital_1101173/Staff_menu.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Staff_menu
-    Private Sub Back_Click(sender As Object, e As EventArgs) Handles Back.Click
-        PageControl.BackPage(Me)
-
-    End Sub
-End Class
