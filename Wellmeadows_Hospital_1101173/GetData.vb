07998b94ad2400855273d7c9233d85818df3288a
TheBatfather <B6501730@g.sut.ac.th>
2023-09-27 22:42:33 +0700

    fix bug date

--- Wellmeadows_Hospital_1101173/GetData.vb
+++ Wellmeadows_Hospital_1101173/GetData.vb
@@ -68,4 +68,30 @@ Module GetData
         Console.WriteLine("ss" & column)
         Return column
     End Function
+
+    Public Function RegisStaff(ByVal data As String) As Int32
+        Dim col() As String = {"STAFF_NUM", "POSITION", "STAFF_NAME", "ADDRESS", "DOB", "SEX", "WORK_LOCATION", "SALARY", "NIN", "TELL", "WORKED_PER_WEEK", "WORK_TYPE", "PAYMENT_TYPE", "SALARY_LEVEL"}
+        'Dim column As String = String.Join(",", )
+
+        Try
+            Dim connection As New OracleConnection(Connect())
+            connection.Open()
+            Dim sql As String = $"INSERT INTO STAFF ({String.Join(",", col)}) VALUES ({String.Join(",", data)})"
+            Console.WriteLine(sql)
+            Dim cmd As New OracleCommand(sql, connection)
+            cmd.ExecuteNonQuery()
+
+            connection.Close()
+            Return 1
+        Catch ex As OracleException When ex.Number = 1 AndAlso ex.Message.Contains("PATIENTS_UK1")
+            ' จัดการกับข้อผิดพลาดที่เกิดจาก unique constraint violation
+            MessageBox.Show("cid is already exist")
+
+        Catch ex As Exception
+            ' จัดการข้อผิดพลาดที่เกิดขึ้น
+            MessageBox.Show("เกิดข้อผิดพลาดในการเชื่อมต่อกับฐานข้อมูล: " & ex.Message)
+            Return 0
+        End Try
+
+    End Function
 End Module
--- Wellmeadows_Hospital_1101173/StaffRegister.vb
+++ Wellmeadows_Hospital_1101173/StaffRegister.vb
@@ -30,4 +30,27 @@
     Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
 
     End Sub
+
+    Private Sub ptno_TextChanged(sender As Object, e As EventArgs) Handles staff_no.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim sex As String = ""
+        If sexmale.Checked Then
+            sex = "male"
+        ElseIf sexfemale.Checked Then
+            sex = "female"
+        End If
+        Dim data() As String = {$"'{staff_no.Text}'", $"'{position.Text}'", $"'{staff_name.Text}'", $"'{address.Text}'", $"{DOB.Value}", $"'{sex}'",
+            $"'{work_location.Text}'", $"'{salary.Text}'", $"'{NIN.Text}'", $"'{tel.Text}'", $"'{worked_per_week.Text}'", $"'{work_type.Text}'", $"'{payment_type.Text}'", $"'{salary_level.Text}'"}
+        Dim result As Int32 = RegisStaff(String.Join(",", data))
+        If result > 0 Then
+            MessageBox.Show("ลงทะเบียนบุคลากรสำเร็จ")
+        End If
+    End Sub
+
+    Private Sub TextBox14_TextChanged(sender As Object, e As EventArgs) Handles payment_type.TextChanged
+
+    End Sub
 End Class
