dd23ca30ebca9fbb0ac4f887bdfc39fb1098979f
ballalone1234 <ballpol.127@gmail.com>
2023-10-29 02:54:35 +0700

    drug dashbroad begin

--- Wellmeadows_Hospital_1101173/dashboarddrug.vb
+++ Wellmeadows_Hospital_1101173/dashboarddrug.vb
@@ -1,4 +1,6 @@
-﻿Public Class dashboarddrug
+﻿Imports Oracle.ManagedDataAccess.Client
+
+Public Class dashboarddrug
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
 
     End Sub
@@ -66,5 +68,48 @@
 
     Private Sub Chart1_Click_2(sender As Object, e As EventArgs) Handles Chart1.Click
 
+    End Sub
+
+    Private Sub dashboarddrug_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' ตั้งค่าการเชื่อมต่อกับ Oracle Database
+
+        ' สร้าง OracleConnection
+        Dim connection As New OracleConnection(Connect())
+
+        ' SQL Query สำหรับดึงข้อมูลจาก Oracle Database
+        Dim query As String = "SELECT to_char(START_DATE, 'Month') as MONTH  , count(*) as COUNT FROM prescribed GROUP by  to_char(START_DATE, 'Month') 
+"
+
+        ' สร้าง DataTable เพื่อเก็บข้อมูล
+        Dim dataTable As New DataTable()
+
+        Try
+            ' เปิดการเชื่อมต่อกับฐานข้อมูล
+            connection.Open()
+
+            ' สร้าง OracleDataAdapter และเตรียมข้อมูลใน DataTable
+            Using adapter As New OracleDataAdapter(query, connection)
+                adapter.Fill(dataTable)
+            End Using
+
+            ' ตรวจสอบว่ามีข้อมูลใน DataTable หรือไม่
+            If dataTable.Rows.Count > 0 Then
+                ' กำหนดข้อมูลใน Chart
+                Chart1.DataSource = dataTable
+                Chart1.Series("Series1").XValueMember = "MONTH"
+                Chart1.Series("Series1").YValueMembers = "Count"
+                Chart1.Series("Series1").IsValueShownAsLabel = True ' แสดงค่าเป็น Label
+                ' แสดง Chart
+                Chart1.DataBind()
+            Else
+                MessageBox.Show("No data available.")
+            End If
+        Catch ex As Exception
+            ' จัดการข้อผิดพลาด
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            ' ปิดการเชื่อมต่อ
+            connection.Close()
+        End Try
     End Sub
 End Class
